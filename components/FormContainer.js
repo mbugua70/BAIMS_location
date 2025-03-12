bb6056bb96842141981ea78db5648873b0ebc1a2
Code MBU <johnmbugua23code@gmail.com>
2025-03-12 11:29:19 +0300

    debugin formContainer.js file

--- components/FormContainer.js
+++ components/FormContainer.js
@@ -1,113 +1,125 @@
-import { View, Text, StyleSheet, Pressable, KeyboardAvoidingView,TouchableWithoutFeedback, ScrollView, Keyboard} from 'react-native';
-import { useIsFocused, useNavigation } from '@react-navigation/native';
-import { ActivityIndicator, MD2Colors } from 'react-native-paper';
-import React, {useState, useLayoutEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  ScrollView,
+  Keyboard,
+} from "react-native";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
+import React, { useState, useLayoutEffect } from "react";
 
 import Input from "./Input";
 import FlatButton from "../UI/FlatButton";
-import { GlobalStyles } from '../Constants/Globalcolors';
+import { GlobalStyles } from "../Constants/Globalcolors";
 
+const FormContainer = ({
+  onSubmit,
+  credentialsInvalid,
+  isPending,
+  name,
+  password,
+  isUpdating,
+}) => {
+  const [enteredName, setEnteredName] = useState(name ? name : "");
+  const [enteredPassword, setEnteredPassword] = useState("");
 
-const FormContainer = ({ onSubmit, credentialsInvalid, isPending, name, password, isUpdating}) => {
-   const [enteredName, setEnteredName] = useState(name ? name : "");
-    const [enteredPassword, setEnteredPassword] = useState("");
+  const isFocused = useIsFocused();
+  const navigation = useNavigation();
 
-    const isFocused = useIsFocused()
-    const navigation = useNavigation();
+  const { name: nameIsValid, password: passwordIsValid } = credentialsInvalid;
 
-
-    const {
-      name: nameIsValid,
-      password: passwordIsValid
-    } = credentialsInvalid;
-
-    function  updateInputValueHandler(inputType, enteredValue) {
-      switch (inputType) {
-        case 'name':
-          setEnteredName(enteredValue);
-          break;
-        case 'password':
-          setEnteredPassword(enteredValue);
-          break;
-      }
+  function updateInputValueHandler(inputType, enteredValue) {
+    switch (inputType) {
+      case "name":
+        setEnteredName(enteredValue);
+        break;
+      case "password":
+        setEnteredPassword(enteredValue);
+        break;
     }
+  }
 
-    function submitHandler() {
-      onSubmit({
-        name: enteredName,
-        password: enteredPassword
-      });
+  function submitHandler() {
+    onSubmit({
+      name: enteredName,
+      password: enteredPassword,
+    });
 
-
-      if (isUpdating && enteredName !== "") {
-        // navigation.setParams(
-        //    { name: enteredName, phone: enteredPhone, region: enteredRegion },
-        // );
-        navigation.goBack();
-      }
+    if (isUpdating && enteredName !== "") {
+      // navigation.setParams(
+      //    { name: enteredName, phone: enteredPhone, region: enteredRegion },
+      // );
+      navigation.goBack();
     }
+  }
 
-    useLayoutEffect(() => {
-      navigation.setOptions({
-        headerRight: () => {
-          return (
-            <Pressable
-              hitSlop={20}
-              onPress={submitHandler}
-              style={({ pressed }) =>
-                pressed ? [styles.button, styles.pressed] : styles.button
-              }>
-              <Text style={styles.textButton}>Save</Text>
-            </Pressable>
-          );
-        },
-      });
-    }, [navigation, submitHandler]);
-
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <Pressable
+            hitSlop={20}
+            onPress={submitHandler}
+            style={({ pressed }) =>
+              pressed ? [styles.button, styles.pressed] : styles.button
+            }>
+            <Text style={styles.textButton}>Save</Text>
+          </Pressable>
+        );
+      },
+    });
+  }, [navigation, submitHandler]);
 
   return (
-
-     <KeyboardAvoidingView
-          style={{ flex: 1 }}
-          behavior='padding'
-          keyboardVerticalOffset={100}>
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-       <ScrollView contentContainerStyle={styles.container}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior='padding'
+      keyboardVerticalOffset={100}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          contentContainerStyle={styles.container}
           keyboardShouldPersistTaps='handled'>
-
-       <Input
-          label="Phone"
-          onUpdateValue={updateInputValueHandler.bind(this, 'name')}
-          value={enteredName}
-          isInvalid={nameIsValid}
-          icon="phone"
-          keyboardType="numeric"
-        />
-
+          <Input
+            label='Phone'
+            onUpdateValue={updateInputValueHandler.bind(this, "name")}
+            value={enteredName}
+            isInvalid={nameIsValid}
+            icon='phone'
+            keyboardType='numeric'
+          />
 
           <Input
-          label="Password"
-          onUpdateValue={updateInputValueHandler.bind(this, 'password')}
-          value={enteredPassword}
-          isInvalid={passwordIsValid}
-          icon="lock"
-        />
+            label='Password'
+            onUpdateValue={updateInputValueHandler.bind(this, "password")}
+            value={enteredPassword}
+            isInvalid={passwordIsValid}
+            icon='lock'
+          />
 
-
-       {/* button content */}
-        <View style={styles.submitContainer}>
-         {!isUpdating && !isPending &&  <FlatButton onPress={submitHandler}>
-            Login
-         </FlatButton>}
-         {isPending &&  <ActivityIndicator size={32} animating={true} color={MD2Colors.blue700} />}
-        </View>
-    </ScrollView>
-    </TouchableWithoutFeedback>
+          {/* button content */}
+          <View style={styles.submitContainer}>
+            {!isUpdating && !isPending && (
+              <FlatButton onPress={submitHandler}>Login</FlatButton>
+            )}
+            {isPending && (
+              <ActivityIndicator
+                size={32}
+                animating={true}
+                color={MD2Colors.blue700}
+              />
+            )}
+          </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
-export default FormContainer
+export default FormContainer;
 
 const styles = StyleSheet.create({
   container: {
@@ -128,13 +140,12 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     padding: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   pressed: {
     backgroundColor: GlobalStyles.colors.primary50,
     borderRadius: 25,
     opacity: 0.75,
   },
-
-})
+});
