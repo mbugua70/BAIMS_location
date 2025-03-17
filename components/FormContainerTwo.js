e980c4abd403293d38992e09a7612fd1fa3aefef
Code MBU <johnmbugua23code@gmail.com>
2025-03-17 18:38:49 +0300

    fix: changed the createAnimateComponent

--- components/FormContainerTwo.js
+++ components/FormContainerTwo.js
@@ -45,7 +45,7 @@ import Checkbox from "./Checkbox";
 import CheckboxComponent from "./Checkbox";
 import PickerImage from "./PickerImage";
 
-const AnimatedFlatlistComp = Animated.FlatList(FlatList);
+const AnimatedFlatlistComp = Animated.FlatList;
 
 const FormContainerTwo = ({
   isEditing,
