6bae54442a0288422ed4d0843d50926ca75ea796
Code MBU <johnmbugua23code@gmail.com>
2025-03-25 15:21:58 +0300

    added offline screen

---
+++ screens/OfflineScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OfflineScreen = () => {
+  return (
+    <View>
+      <Text>Offline Support Coming Soon</Text>
+    </View>
+  )
+}
+
+export default OfflineScreen
