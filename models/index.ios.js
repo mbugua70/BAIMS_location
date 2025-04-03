2edf255e319063380674fc1d12dabb270068d293
Code MBU <johnmbugua23code@gmail.com>
2025-04-03 14:26:36 +0300

    changed the file to only exercute the code on android and ios

--- models/index.ios.js
+++ models/index.ios.js
@@ -1,11 +1,19 @@
 import { Platform } from 'react-native'
 import { Database } from '@nozbe/watermelondb'
-import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite'
+import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs'
+// import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite'
 
 import schema from './schema'
 import migrations from './migrations'
 import Forms from './forms'
 // import Post from './model/Post' // ⬅️ You'll import your Models here
+
+let SQLiteAdapter;
+
+if (Platform.OS === 'ios' || Platform.OS === 'android') {
+  SQLiteAdapter = require('@nozbe/watermelondb/adapters/sqlite').default;
+}
+
 
 // Conditionally configure the adapter based on platform
 const adapter = Platform.OS === 'ios' || Platform.OS === 'android'
@@ -17,7 +25,39 @@ const adapter = Platform.OS === 'ios' || Platform.OS === 'android'
         // Database failed to load -- offer the user to reload the app or log out
       }
     })
-  : null;  // Or use a different adapter for Web (if required)
+  : new LokiJSAdapter({
+    schema,
+    // (You might want to comment out migrations for development purposes -- see Migrations documentation)
+    migrations,
+    useWebWorker: false,
+    useIncrementalIndexedDB: true,
+    // dbName: 'myapp', // optional db name
+
+    // --- Optional, but recommended event handlers:
+
+    onQuotaExceededError: (error) => {
+      // Browser ran out of disk space -- offer the user to reload the app or log out
+    },
+    onSetUpError: (error) => {
+      // Database failed to load -- offer the user to reload the app or log out
+    },
+    extraIncrementalIDBOptions: {
+      onDidOverwrite: () => {
+        // Called when this adapter is forced to overwrite contents of IndexedDB.
+        // This happens if there's another open tab of the same app that's making changes.
+        // Try to synchronize the app now, and if user is offline, alert them that if they close this
+        // tab, some data may be lost
+      },
+      onversionchange: () => {
+        // database was deleted in another browser tab (user logged out), so we must make sure we delete
+        // it in this tab as well - usually best to just refresh the page
+        if (checkIfUserIsLoggedIn()) {
+          window.location.reload()
+        }
+      },
+    }
+  })
+  ;  // Or use a different adapter for Web (if required)
 
 // Only initialize the database if the adapter is valid
 export const database = adapter ? new Database({
